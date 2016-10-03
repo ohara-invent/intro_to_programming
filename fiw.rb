21706b7ef24c3401f1bee8930d777fa35b0f5995
Peter O'Hara Adu <ohara.invent@gmail.com>
2016-10-03 08:41:02 +0000

    Works up to the tens of thousands

--- fiw.rb
+++ fiw.rb
@@ -1,48 +1,130 @@
 #!/usr/bin/ruby
 
-def fiw(number)
-	dictionary = Hash[
-		0 => "",
-		1 => "one",
-		2 => "two",
-		3 => "three",
-		4 => "four",
-		5 => "five",
-		6 => "six",
-		7 => "seven",
-		8 => "eight",
-		9 => "nine",
-		10 => "ten",
-		11 => "eleven",
-		12 => "twelve",
-		13 => "thirteen",
-		14 => "fourteen",
-		15 => "fifteen",
-		16 => "sixteen",
-		17 => "seventeen",
-		18 => "eighteen",
-		19 => "nineteen",
-		20 => "twenty",
-		30 => "thirty",
-		40 => "forty",
-		50 => "fifty",
-		60 => "sixty",
-		70 => "seventy",
-		80 => "eighty",
-		90 => "ninety",
-		100 => "hundred",
-		1000 => "thousand",
-		1000000 => "million",
-		1000000000 => "billion"
-	]
+def main(test_nums)
+	test_nums.each { |num|
+		print num
+		print ": "
+		print nums_to_words(num)
+		print "\n"
+	}
+end
 
-	return "negative " + fiw(number.abs) if number < 0
-	return dictionary[number] if number <= 20
-	return dictionary[(number / 10) * 10] + " " + fiw(number % 10) if number > 20
-	return "zero" if number == 0 # TODO: Bring back zero
+def nums_to_words(number)
+	# puts number
+	if (number == 0)
+		return "zero"
+	elsif (number <= 9)
+		return extract_ones(number)
+	elsif (number < 100)
+		return extract_tens(number)
+	elsif (number < 1000)
+		return extract_hundreds(number)
+	else
+		return extract_thousands(number)
+	end
+end
+
+def extract_thousands(number)
+	if (number % 1000 == 0)
+		return extract_tens(number / 1000) + " thousand"
+	elsif (number % 1000 < 100 )
+		return extract_tens(number / 1000) + " thousand and " + extract_tens(number % 1000)
+	else
+		return extract_tens(number / 1000) + " thousand, " + extract_hundreds(number % 1000)
+	end		
 end
 
 
-(-99..99).each { |number|
-	puts "#{number}: #{fiw(number)}"
-}
+def extract_hundreds(number)
+	if (number % 100 == 0)
+		return extract_ones(number / 100) + " hundred"
+	else
+		return extract_ones(number / 100) + " hundred and " + extract_tens(number % 100)
+	end		
+end
+
+
+def extract_tens(number)
+	tens = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+	if (number < 10)
+		return extract_ones(number)
+	elsif (number < 20)
+		return extract_teens(number)
+	elsif (number <= 100)
+		return tens[number / 10] + " " + extract_ones(number % 10)
+	end
+end
+
+def extract_teens(number)
+	teens = %w(ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen)
+	return teens[number - 10]
+end
+
+
+def extract_ones(number)
+	ones  = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+	return ones[number]
+end
+
+
+test_nums = [
+	0,
+	6,
+
+	10,
+	16,
+
+	20,
+	26,
+
+	100,
+	106,
+	110,
+	116,
+	120,
+	126,
+
+	1000,
+	1006,
+	1010,
+	1016,
+	1020,
+	1026,
+
+	1100,
+	1106,
+	1110,
+	1116,
+	1120,
+	1126,
+
+	13100,
+	13106,
+	13110,
+	13116,
+	13120,
+	13126,
+
+	# 131000,
+	# 131006,
+	# 131010,
+	# 131016,
+	# 131020,
+	# 131026,
+
+	# 1301100,
+	# 1301106,
+	# 1301110,
+	# 1301116,
+	# 1301120,
+	# 1301126,
+
+	# 13453100,
+	# 13453106,
+	# 13453110,
+	# 13453116,
+	# 13453120,
+	# 13453126
+]
+
+main(test_nums)
